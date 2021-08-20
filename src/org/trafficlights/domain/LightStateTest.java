0f0fec83992f475d6702103bb2d82df79c14d8f1
Matthew M <MatthewFMcAndrews@gmail.com>
2021-08-19 20:35:06 -0500

    Start implementing LightState as an enum. Passing first test.

---
+++ src/org/trafficlights/domain/LightStateTest.java
@@ -0,0 +1,12 @@
+package org.trafficlights.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LightStateTest {
+    @Test
+    public void testStateChange() {
+        assertEquals(LightState.RED_YELLOW, LightState.RED.next());
+    }
+}
