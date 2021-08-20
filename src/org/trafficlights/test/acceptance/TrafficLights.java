8f30f762b2b10019b8f5b3a18be9b8375f54f076
Matthew M <MatthewFMcAndrews@gmail.com>
2021-08-19 21:54:36 -0500

    Use editor to convert between Fitnesse test data and the real java API.

--- src/org/trafficlights/test/acceptance/TrafficLights.java
+++ src/org/trafficlights/test/acceptance/TrafficLights.java
@@ -1,15 +1,13 @@
 package org.trafficlights.test.acceptance;
 
+import org.trafficlights.domain.LightState;
+
 public class TrafficLights {
-    private String state;
-    public void setPreviousState(String state) {
+    private LightState state;
+    public void setPreviousState(LightState state) {
         this.state = state;
     }
-    public String nextState() {
-        if ("red".equals(state)) return "red, yellow";
-        if ("red, yellow".equals(state)) return "green";
-        if ("green".equals(state)) return "yellow";
-        if ("yellow".equals(state)) return "red";
-        return "yellow blink";
+    public LightState nextState() {
+        return state.next();
     }
 }
