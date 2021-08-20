249b94a54b4a62dff861674cc784d26aa6c0c334
Matthew M <MatthewFMcAndrews@gmail.com>
2021-08-19 21:56:20 -0500

    Formatting

--- src/org/trafficlights/test/acceptance/TrafficLights.java
+++ src/org/trafficlights/test/acceptance/TrafficLights.java
@@ -3,10 +3,13 @@ package org.trafficlights.test.acceptance;
 import org.trafficlights.domain.LightState;
 
 public class TrafficLights {
+
     private LightState state;
+
     public void setPreviousState(LightState state) {
         this.state = state;
     }
+
     public LightState nextState() {
         return state.next();
     }
