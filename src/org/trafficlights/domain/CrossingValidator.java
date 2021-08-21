26f27214a7b9162251a1e38d7c30373fb2539c53
Matthew M <MatthewFMcAndrews@gmail.com>
2021-08-21 15:05:42 -0500

    More tests and refactoring

---
+++ src/org/trafficlights/domain/CrossingValidator.java
@@ -0,0 +1,13 @@
+package org.trafficlights.domain;
+
+public class CrossingValidator {
+    public boolean isValidConfiguration(LightState firstState, LightState secondState) {
+        if (LightState.UNKNOWN.equals(firstState)) {
+            return false;
+        }
+        if (LightState.RED.equals(secondState)) {
+            return true;
+        }
+        return LightState.RED.equals(firstState) && LightState.GREEN.equals(secondState);
+    }
+}
--- src/org/trafficlights/test/acceptance/FirstLightSwitchingCrossingController.java
+++ src/org/trafficlights/test/acceptance/FirstLightSwitchingCrossingController.java
@@ -1,47 +1,39 @@
 package org.trafficlights.test.acceptance;
 
 import org.trafficlights.domain.LightState;
+import org.trafficlights.domain.CrossingValidator;
 
 public class FirstLightSwitchingCrossingController {
-
-    LightState firstState;
-    LightState secondState;
-
+    private LightState firstState;
+    private LightState secondState;
     public void setFirstLight(LightState state) {
         this.firstState = state;
     }
-
     public void setSecondLight(LightState state) {
         this.secondState = state;
     }
-
     public LightState firstLight() {
         return firstState;
     }
-
     public LightState secondLight() {
         return secondState;
     }
-
     public void execute() {
         switchFirstLight();
     }
-
+    private final CrossingValidator validator = new CrossingValidator();
     public void switchFirstLight() {
-        if (!isValidLightStateConfiguration()) {
+        if (!validator.isValidConfiguration(firstState, secondState)) {
             warningConfiguration();
             return;
         }
         firstState = firstState.next();
+        if (!validator.isValidConfiguration(firstState, secondState)) {
+            warningConfiguration();
+        }
     }
-
     private void warningConfiguration() {
         firstState = LightState.UNKNOWN;
         secondState = LightState.UNKNOWN;
     }
-
-    private boolean isValidLightStateConfiguration() {
-        return !LightState.UNKNOWN.equals(firstState) && LightState.RED.equals(secondState);
-    }
-
 }
